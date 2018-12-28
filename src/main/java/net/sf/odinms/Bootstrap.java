d8efb0077e8e5b3beb4eec5cb9d4c25b6b708f2b
zhangyi02 <zhangyi02@daojia-inc.com>
2018-12-28 09:36:14 +0800

    buddy

---
+++ src/main/java/net/sf/odinms/Bootstrap.java
@@ -0,0 +1,17 @@
+package net.sf.odinms;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author EasyZhang
+ * @date 2018/12/27 -  18:28
+ */
+
+public class Bootstrap {
+
+    private static final Logger logger = LoggerFactory.getLogger(Bootstrap.class);
+    public static void main(String[] args){
+
+    }
+}
